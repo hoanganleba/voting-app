e4e5319ff8fd3e5ad8b9cfdc4077376ed864bc45
Lê Bá Hoàng An <hoanganleba@gmail.com>
2021-08-19 19:04:43 +0700

    Remove MongoURI

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env: {
-    MONGO_URI: "mongodb+srv://hoanganleba:megolio1998@cluster0.npceu.mongodb.net/test?retryWrites=true&w=majority"
-  }
 }
